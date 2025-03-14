b88be4dc909f29d89db5adea8447460db786e778
Edwin-Aguirre <edwina3@uci.edu>
2025-03-14 16:46:04 -0700

    Added Plane Animation and Pipe Scene

--- scripts/plane.gd
+++ scripts/plane.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 var jump_force : float = -350.0
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,9 +17,19 @@ func _process(_delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	
-	if (Input.is_action_just_pressed("jump")):
-		velocity.y = jump_force
+	fly(delta)
 	
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
+
+func fly(delta) -> void:
+	velocity.y += gravity * delta
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force
+		animation_player.play("jump")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
