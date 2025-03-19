3acd8effb594dbb00aa580337f77220362acf958
Edwin-Aguirre <edwina3@uci.edu>
2025-03-18 18:32:27 -0700

    Added Scene Transitions

--- globals/game_manager.gd
+++ globals/game_manager.gd
@@ -3,11 +3,22 @@ extends Node
 
 const MAIN = preload("res://scenes/main.tscn")
 const GAME = preload("res://scenes/game.tscn")
+const TRANSITION = preload("res://scenes/transition.tscn")
 
+var next_scene: PackedScene
+
+
+func add_transition() -> void:
+	var transition = TRANSITION.instantiate()
+	add_child(transition)
 
 func load_game_scene() -> void:
-	get_tree().change_scene_to_packed(GAME)
+	next_scene = GAME
+	add_transition()
+	#get_tree().change_scene_to_packed(TRANSITION)
 
 
 func load_main_scene() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	next_scene = MAIN
+	add_transition()
+	#get_tree().change_scene_to_packed(TRANSITION)
